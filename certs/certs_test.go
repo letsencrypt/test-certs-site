c98b27df294e6a84e65e67a716b5982f319907b1
Matthew McPherrin <git@mcpherrin.ca>
2025-11-26 14:05:33 -0500

    Add test for expiry handling
    Make sure all four possible cases are covered.

--- certs/certs_test.go
+++ certs/certs_test.go
@@ -2,10 +2,13 @@ package certs
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/asn1"
 	"slices"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/go-acme/lego/v4/challenge/tlsalpn01"
 
@@ -66,5 +69,84 @@ func TestACME(t *testing.T) {
 	_, err = manager.GetCertificate(&clientHello)
 	if err == nil {
 		t.Fatal("Expected error after cleanup, got none")
+	}
+}
+
+// TestExpiredHandling checks the four possible cases with expiry handling.
+// A cert can either be expired or not, and it should be expired or not.
+func TestExpiredHandling(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		name            string
+		NotAfter        time.Time
+		shouldBeExpired bool
+		shouldErr       bool
+	}{
+		{
+			name:            "should-be-expired.is-expired",
+			NotAfter:        time.Now().Add(-time.Hour),
+			shouldBeExpired: true,
+			shouldErr:       false,
+		},
+		{
+			name:            "should-not-be-expired.is-expired",
+			NotAfter:        time.Now().Add(-time.Hour),
+			shouldBeExpired: false,
+			shouldErr:       true,
+		},
+		{
+			name:            "should-be-expired.not-expired",
+			NotAfter:        time.Now().Add(time.Hour),
+			shouldBeExpired: true,
+			shouldErr:       true,
+		},
+		{
+			name:            "should-not-be-expired.not-expired",
+			NotAfter:        time.Now().Add(time.Hour),
+			shouldBeExpired: false,
+			shouldErr:       false,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			cm := CertManager{
+				mu: sync.Mutex{},
+				certs: map[string]cert{
+					tc.name: {
+						shouldBeExpired: tc.shouldBeExpired,
+						it: &tls.Certificate{
+							Leaf: &x509.Certificate{
+								NotAfter: tc.NotAfter,
+								DNSNames: []string{tc.name},
+							},
+						},
+					},
+				},
+			}
+
+			c, err := cm.GetCertificate(&tls.ClientHelloInfo{
+				ServerName: tc.name,
+			})
+
+			if tc.shouldErr && err == nil {
+				t.Fatal("Expected error, got none")
+			}
+
+			if !tc.shouldErr {
+				if err != nil {
+					t.Fatalf("Didn't expect error, got %v", err)
+				}
+
+				if c == nil {
+					t.Fatal("Expected non-nil Certificate")
+				}
+
+				if c.Leaf.DNSNames[0] != tc.name {
+					t.Fatalf("Expected DNS name to be %q, got %q", tc.name, c.Leaf.DNSNames[0])
+				}
+			}
+		})
 	}
 }
