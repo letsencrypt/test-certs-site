dd0d03c285c09e34b74667f971098cc70e972bfc
Matthew McPherrin <git@mcpherrin.ca>
2025-12-18 12:49:08 -0500

    Make it clearer that this is for use with container/heap

--- scheduler/job_heap.go
+++ scheduler/job_heap.go
@@ -1,22 +1,31 @@
 package scheduler
 
+import "container/heap"
+
 // jobHeap implements a heap sorted by job.at
 // This is nearly verbatim taken from the container/heap example.
 // Hopefully some day it can be replaced by a generic heap.
 type jobHeap []job
 
+// jobHeap implements heap.Interface
+var _ heap.Interface = (*jobHeap)(nil)
+
+// Len is required for heap.Interface
 func (h *jobHeap) Len() int {
 	return len(*h)
 }
 
+// Less is required for heap.Interface
 func (h *jobHeap) Less(i, j int) bool {
 	return (*h)[i].at.Before((*h)[j].at)
 }
 
+// Swap is required for heap.Interface
 func (h *jobHeap) Swap(i, j int) {
 	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
+// Push is required for heap.Interface
 func (h *jobHeap) Push(x any) {
 	typed, ok := x.(job)
 	if !ok {
@@ -26,6 +35,7 @@ func (h *jobHeap) Push(x any) {
 	*h = append(*h, typed)
 }
 
+// Pop is required for heap.Interface
 func (h *jobHeap) Pop() any {
 	old := *h
 	n := len(old)
