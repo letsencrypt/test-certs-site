e5f087b426cb62fe90b400161419a05644f9a5da
Matthew McPherrin <git@mcpherrin.ca>
2026-01-08 20:49:19 -0500

    pull out takeNext function

--- acme/issuer.go
+++ acme/issuer.go
@@ -1,6 +1,7 @@
 package acme
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log/slog"
 	"time"
@@ -40,7 +41,7 @@ func (i *issuer) start() {
 	curr, err := i.store.ReadCurrent(i.domain)
 	if err != nil {
 		i.logger.Error("reading current certificate", slogErr(err))
-		// renewAt is zero, will renew
+		// If we failed to read, leave renewAt zero, and we'll issue a new cert
 	} else {
 		renewAt = i.checkRenew(curr.Leaf)
 	}
@@ -61,25 +62,26 @@ func (i *issuer) start() {
 	i.schedule.RunAt(renewAt, func() { i.start() })
 }
 
+// issue the next certificate, then take it.
 func (i *issuer) issue() error {
 	// Check if there's a next certificate already in progress
 	next, err := i.store.ReadNext(i.domain)
 	if err != nil {
 		i.logger.Info("couldn't read next certificate so issuing", slogErr(err))
 
-		err := i.issueNext()
+		next, err = i.issueNext()
 		if err != nil {
 			return err
 		}
-
-		return i.issue()
 	}
 
 	readyTime, err := i.checkReady(next.Leaf)
 	if err != nil {
 		i.logger.Error("checking ready certificate", slogErr(err))
 
-		err := i.issueNext()
+		// checkReady can return an error if the current "next" cert is broken (eg, expired)
+		// and so we need to issue a new one and start over.
+		next, err = i.issueNext()
 		if err != nil {
 			return err
 		}
@@ -88,19 +90,9 @@ func (i *issuer) issue() error {
 	}
 
 	if time.Now().After(readyTime) {
-		i.logger.Info("next certificate is ready")
-		// Next cert is ready! Take it.
-		_, err := i.store.TakeNext(i.domain)
+		err := i.takeNext()
 		if err != nil {
-			i.logger.Error("taking next certificate", slogErr(err))
-			// If this failed for some reason, reissue
-			return i.issueNext()
-		}
-		err = i.manager.LoadCertificate(i.domain)
-		if err != nil {
-			i.logger.Error("loading certificate", slogErr(err))
-
-			return i.issueNext()
+			return err
 		}
 
 		i.logger.Info("certificate issuance completed")
@@ -119,11 +111,11 @@ func (i *issuer) issue() error {
 }
 
 // issueNext is called to actually issue the next certificate
-func (i *issuer) issueNext() error {
+func (i *issuer) issueNext() (tls.Certificate, error) {
 	i.logger.Info("issuing new next certificate")
 	key, err := i.store.StoreNextKey(i.domain, i.keyType)
 	if err != nil {
-		return fmt.Errorf("could not store next key: %w", err)
+		return tls.Certificate{}, fmt.Errorf("could not store next key: %w", err)
 	}
 	resp, err := i.client.Certificate.Obtain(certificate.ObtainRequest{
 		Profile:        i.profile,
@@ -133,7 +125,7 @@ func (i *issuer) issueNext() error {
 		PreferredChain: i.issuerCN,
 	})
 	if err != nil {
-		return fmt.Errorf("could not obtain certificate: %w", err)
+		return tls.Certificate{}, fmt.Errorf("could not obtain certificate: %w", err)
 	}
 
 	if i.shouldRevoke() {
@@ -142,16 +134,33 @@ func (i *issuer) issueNext() error {
 		err := i.client.Certificate.RevokeWithReason(resp.Certificate, &reasonKeyCompromise)
 		if err != nil {
 			// TODO: if we failed to revoke, we should probably retry revoking
-			return fmt.Errorf("could not revoke certificate: %w", err)
+			return tls.Certificate{}, fmt.Errorf("could not revoke certificate: %w", err)
 		}
 	}
 
 	err = i.store.StoreNextCert(i.domain, resp.Certificate)
 	if err != nil {
-		return fmt.Errorf("could not store next certificate: %w", err)
+		return tls.Certificate{}, fmt.Errorf("could not store next certificate: %w", err)
 	}
 
 	i.logger.Info("next certificate issued", slog.String("domain", i.domain))
+
+	return i.store.ReadNext(i.domain)
+}
+
+// takeNext checks if the next certificate is ready, and takes it if so
+// It returns the time to start() next
+func (i *issuer) takeNext() error {
+	i.logger.Info("next certificate is ready")
+	// Next cert is ready! Take it.
+	_, err := i.store.TakeNext(i.domain)
+	if err != nil {
+		return err
+	}
+	err = i.manager.LoadCertificate(i.domain)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
