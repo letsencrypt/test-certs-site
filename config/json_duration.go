2b8ed4daf816dd64e9d207589a9f208ebcd73dd5
Matthew McPherrin <git@mcpherrin.ca>
2026-04-02 17:00:58 -0400

    comment for exported method

--- config/json_duration.go
+++ config/json_duration.go
@@ -8,6 +8,7 @@ import (
 // Duration adds JSON unmarshalling to time.Duration
 type Duration time.Duration
 
+// UnmarshalJSON as a string, parsing with time.ParseDuration
 func (d *Duration) UnmarshalJSON(bytes []byte) error {
 	var str string
 	if err := json.Unmarshal(bytes, &str); err != nil {
